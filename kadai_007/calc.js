610d0d2d01fb82a10885a01fca3b59f54fbf1746
yakisoba-ya <143864653+yakisoba-ya@users.noreply.github.com>
2023-10-21 20:15:22 +0900

    Add files via upload

---
+++ kadai_007/calc.js
@@ -0,0 +1,15 @@
+let num = 100;
+
+
+if ((num % 3 === 0)&&(num % 5 === 0)){
+  console.log('３と５の倍数です');
+
+}else if (num % 5 === 0){
+  console.log ('5の倍数です');
+
+}else if (num % 3 === 0 ){
+  console.log('3の倍数です');
+
+}else {
+  console.log(num);
+}
