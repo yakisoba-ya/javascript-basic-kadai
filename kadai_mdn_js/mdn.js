b9d4214c0655398cc1f56b56ef95d5ddab3de6f7
yakisoba-ya <143864653+yakisoba-ya@users.noreply.github.com>
2023-10-23 10:42:44 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,9 @@
+const day = new Date('september 22, 2023');
+const date1 = day.getDate();
+const date2 = day.getFullYear();
+const date3 = day.getMonth();
+
+
+console.log(date2);
+console.log(date3);
+console.log(date1);
