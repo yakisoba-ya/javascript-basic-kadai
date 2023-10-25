da255c7d27856d63f22cb8b01f152fa7bc8617b7
yakisoba-ya <143864653+yakisoba-ya@users.noreply.github.com>
2023-10-25 10:10:35 +0900

    Add files via upload

---
+++ kadai_021/event.js
@@ -0,0 +1,16 @@
+const btn =document.getElementById('btn')
+
+
+
+btn.addEventListener('click', () => {
+  setTimeout(() =>{
+const newText = document.getElementById('text')
+
+newText.textContent='ボタンをクリックしました'
+
+},2000)
+
+})
+
+
+
